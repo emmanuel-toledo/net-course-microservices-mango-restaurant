8402d618de08350d2bf1d1241a14815c3a649785
Emmanuel Toledo <emmanueltoledo1082@gmail.com>
2023-04-10 19:01:32 -0600

    Created Identity Server microservice with NuGet packages. Configured ApplicationDbContext and ApplicationUser to connect to a Db with Entity Framework

---
+++ MangoRestaurant/Mango.Service.Identity/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Service.Identity.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Service.Identity.DbContexts
+{
+    public class ApplicationDbContext : IdentityDbContext<AplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+}
---
+++ MangoRestaurant/Mango.Service.Identity/Models/AplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Mango.Service.Identity.Models
+{
+    public class AplicationUser : IdentityUser
+    {
+    }
+}
