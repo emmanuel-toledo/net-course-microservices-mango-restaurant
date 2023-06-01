f0977d80b0c98ffceddf0a46c9c7adbe30517146
Emmanuel Toledo <emmanueltoledo1082@gmail.com>
2023-06-01 14:07:23 -0600

    Updated comments.

--- Mango.Services.Coupon.Web.Api/Models/Dto/CouponDto.cs
+++ Mango.Services.Coupon.Web.Api/Models/Dto/CouponDto.cs
@@ -1,13 +1,28 @@
 ﻿namespace Mango.Services.Coupon.Web.Api.Models.Dto
 {
+    /// <summary>
+    /// This is the user object that will be used to return and receive information of "Coupon" table.
+    /// </summary>
     public class CouponDto
     {
+        /// <summary>
+        /// Get and set unique identifier.
+        /// </summary>
         public int CouponId { get; set; }
 
+        /// <summary>
+        /// Get and set coupon code.
+        /// </summary>
         public string CouponCode { get; set; }
 
+        /// <summary>
+        /// Get and set discount amount.
+        /// </summary>
         public double DiscountAmount { get; set; }
 
+        /// <summary>
+        /// Get and set minimum amount to apply this coupon.
+        /// </summary>
         public int MinAmount { get; set; }
     }
 }
--- Mango.Services.Coupon.Web.Api/Models/Dto/ResponseDto.cs
+++ Mango.Services.Coupon.Web.Api/Models/Dto/ResponseDto.cs
@@ -1,11 +1,23 @@
 ﻿namespace Mango.Services.Coupon.Web.Api.Models.Dto
 {
+    /// <summary>
+    /// This is the main response object to each call to the service.
+    /// </summary>
     public class ResponseDto
     {
+        /// <summary>
+        /// Get and set the generic result of the request.
+        /// </summary>
         public object? Result { get; set; }
 
+        /// <summary>
+        /// Get and set the is success flag.
+        /// </summary>
         public bool IsSuccess { get; set; } = true;
 
+        /// <summary>
+        /// Get and set the error message.
+        /// </summary>
         public string Message { get; set; } = string.Empty;
     }
 }
